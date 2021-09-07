8ebb7e07c5dd23f2688a91035e6f397297800ca9
Sajan <sajanmhatre29@gmail.com>
2021-09-08 02:19:56 +0530

    Insert Elements in Queue

--- LinkedList Stack and Queue/src/QueueUsingLinkedList.java
+++ LinkedList Stack and Queue/src/QueueUsingLinkedList.java
@@ -1,5 +1,58 @@
-public class QueueUsingLinkedList {
-    public static void main(String[] args) {
-        System.out.println("Welcome to Queue....");
+// A Linked List Node
+class Node{
+    int data;
+    Node next;
+    public Node(int data){
+        this.data = data;
+        this.next = null;
+    }
+}
+class QueueUsingLinkedList{
+    private static Node rear = null, front = null;
+    // Method to add an element to the queue
+    public static void enqueue(int element){
+        // Create a New Node named newNode
+        Node newNode = new Node(element);
+        System.out.println(element+" Inserted in Queue");
+        //case: queue was empty
+        if (front == null){
+            // initialize both front and rear
+            front = newNode;
+            rear = newNode;
+        }
+        else {
+            // update rear
+            rear.next = newNode;
+            rear = newNode;
+        }
+    }
+    // Method to return the top element in a queue
+    public static int peek()
+    {
+        if (front != null) {
+            return front.data;
+        }
+        else {
+            System.exit(1);
+        }
+        return -1;
+    }
+
+    // Method to check if the queue is empty or not
+    public static void isEmpty() {
+//        return rear == null && front == null;
+        if ((rear == null && front == null)) {
+            System.out.println("The queue is empty");
+        }
+        else {
+            System.out.println("The queue is not empty");
+        }
+    }
+    public static void main(String[] args){
+        QueueUsingLinkedList q = new QueueUsingLinkedList();
+        q.enqueue(56);
+        q.enqueue(30);
+        q.enqueue(70);
+        System.out.println("The front element of Queue is: "+peek());
     }
 }
